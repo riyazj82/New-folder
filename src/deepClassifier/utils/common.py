870d201a92f28834aba307f582108e4e6a9f50ce
Riyaz Jaleel <riyazj82@gmail.com>
2023-02-02 09:58:38 +0530

    commit

--- src/deepClassifier/utils/common.py
+++ src/deepClassifier/utils/common.py
@@ -6,7 +6,8 @@ import json
 import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
-
+from pathlib import Path
+from typing import Any
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
